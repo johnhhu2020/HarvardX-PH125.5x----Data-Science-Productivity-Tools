415c46dbc0b2e43900511e962c6db66dcee4e05f
johnhhu2020 <john.hhu2020@gmail.com>
2022-08-29 19:07:06 +0800

    download.file(url, destfile = dest_file,  method = "libcurl") # https://stackoverflow.com/questions/71271210/r-markdown-cannot-open-url-when-using-download-file

--- download-data.R
+++ download-data.R
@@ -1,3 +1,4 @@
 url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
 dest_file <- "data/murders.csv"
-download.file(url, destfile = dest_file)
+download.file(url, destfile = dest_file,  method = "libcurl")
+# https://stackoverflow.com/questions/71271210/r-markdown-cannot-open-url-when-using-download-file
